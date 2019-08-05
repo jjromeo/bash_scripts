b47da705a3b77a6ed1f838d8b8e14431cab2794e
Jerome Pratt <jerome.pratt@babylonhealth.com>
2019-08-05 16:36:18 +0100

    Small updates

--- vmux.bash
+++ vmux.bash
@@ -1,11 +1,15 @@
 # Aliases
 alias tat="tmux attach -t"
 alias bashp="vim ~/.bash_profile"
+alias bashr="vim ~/.bashrc"
 alias bscripts="vim ~/.bash_scripts"
 alias sb="source ~/.bash_profile"
 alias ctb="ctags -R --languages=ruby --exclude=.git --exclude=log . $(bundle list --paths)"
 alias vi="vim"
 alias vrc="vi ~/.vimrc"
+alias tconf="vi ~/.tmux.conf"
 #Keep editor set (mainly for git text input)
 export EDITOR="vim"
 export PATH=$PATH:$(pwd)
+# Set up jump
+eval "$(jump shell)"
